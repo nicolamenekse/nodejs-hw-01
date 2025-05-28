91be45b2b2ca712d1289e19f49a83ca70d88a4a6
Nicola <menekse776@gmail.com>
2025-05-28 23:21:38 +0300

    addedScripts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,19 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    try{
+        const contacts = await readContacts();
+
+        if(contacts.length > 0){
+            await writeContacts([]);
+            console.log("hepsi silindi");
+        }else{
+            console.log("zaten booos");
+        }
+    }catch(err){
+        console.log(err);
+    }
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    try{
+        const contacts = await readContacts();
+        if(contacts.length ===0){
+            console.log("Dizi bos");
+            return;
+        }
+
+        const removeLastContact = contacts.pop();
+        await writeContacts(contacts);
+        console.log(removeLastContact);
+    }catch(err){
+        console.log(err);
+    }
+};
 
 removeLastContact();
